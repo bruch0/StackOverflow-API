1f4f1350fd0dcd6f67a49f9b19a65b525da3995d
bruch0 <lucas.bruch0@gmail.com>
2021-12-09 15:56:18 -0300

    feat: add new user error

--- src/errors/user.ts
+++ src/errors/user.ts
@@ -5,4 +5,11 @@ class InvalidUser extends Error {
   }
 }
 
-export { InvalidUser };
+class UserNotFound extends Error {
+  constructor() {
+    super('Usuário inválido, cheque se os seus parâmetros estão corretos');
+    this.name = 'userNotFound';
+  }
+}
+
+export { InvalidUser, UserNotFound };
