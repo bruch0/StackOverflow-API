1417e350907f6004153edce4eaa24ea912cc87e4
bruch0 <lucas.bruch0@gmail.com>
2021-12-09 09:12:55 -0300

    fix: add missing columns fillment

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,7 +1,10 @@
 import connection from '../database/database';
 
-const createUser = async ({ string: username }) => {
-  await connection.query('INSERT INTO users (name) VALUES ($1)', [username]);
+const createUser = async ({ string: username, number: classId }) => {
+  await connection.query(
+    'INSERT INTO users (name, class_id, answers, total_score) VALUES ($1, $2, 0, 0)',
+    [username, classId]
+  );
 };
 
 export { createUser };
