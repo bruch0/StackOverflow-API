6b27c2dce9a4cb89b992c83375f8b9397792a9bf
bruch0 <lucas.bruch0@gmail.com>
2021-12-09 15:57:18 -0300

    feat: add new interface, add a return on the user id

--- src/repositories/questionRepository.ts
+++ src/repositories/questionRepository.ts
@@ -1,14 +1,16 @@
 import connection from '../database/database';
 
-import { Question } from '../interfaces/questionInterface';
+import { QuestionOnRepository } from '../interfaces/questionInterface';
 
-const registerQuestion = async (questionInfo: Question) => {
+const registerQuestion = async (questionInfo: QuestionOnRepository) => {
   const { userId, userClassId, question, tags, submitionDate } = questionInfo;
 
-  await connection.query(
-    'INSERT INTO questions (user_id, user_class_id, question, tags, submition_date, answer, score) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+  const questionId = await connection.query(
+    'INSERT INTO questions (user_id, user_class_id, question, tags, submition_date, answer, score) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id',
     [userId, userClassId, question, tags, submitionDate, '', 0]
   );
+
+  return questionId.rows[0].id;
 };
 
 export { registerQuestion };
