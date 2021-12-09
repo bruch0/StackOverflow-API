21f5c857400a299db63f0929f97724775902defc
bruch0 <lucas.bruch0@gmail.com>
2021-12-09 10:49:38 -0300

    feat: added all the previous features to the controller

---
+++ src/controllers/userController.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+
+import * as userService from '../services/userService';
+
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, classId } = req.body;
+
+  if (!name || !classId)
+    return res
+      .status(400)
+      .send(
+        'Requisição inválida, insira um nome entre três e 20 caractéres e uma classe válida'
+      );
+
+  try {
+    const token = await userService.createUser(req.body);
+
+    return res.send({ token });
+  } catch (error: any) {
+    if (error.name === 'invalidUser')
+      return res.status(400).send(error.message);
+
+    next(error);
+  }
+};
+
+export { createUser };
