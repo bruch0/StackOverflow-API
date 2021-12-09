31307af2612e03c3e7c6b3354a93a6684083874a
bruch0 <lucas.bruch0@gmail.com>
2021-12-09 15:59:11 -0300

    feat: add createQuestion to the app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 import userRoute from './routes/user';
+import questionRoute from './routes/question';
 import inexistentRoute from './routes/error';
 import databaseError from './middlewares/databaseError';
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(userRoute);
+app.use(questionRoute);
 
 app.use(databaseError);
 app.use(inexistentRoute);
