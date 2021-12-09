dc39546dbae236c4466f86c73e0afcfdb677f5f7
bruch0 <lucas.bruch0@gmail.com>
2021-12-09 15:57:43 -0300

    feat: add function to get the user info needed by the token

--- src/repositories/tokenRepository.ts
+++ src/repositories/tokenRepository.ts
@@ -7,4 +7,25 @@ const regiserUserToken = async (userId: number, token: string) => {
   );
 };
 
-export { regiserUserToken };
+const getUserInfoByToken = async (userToken: string) => {
+  const user = await connection.query(
+    'SELECT * FROM user_tokens WHERE token = $1',
+    [userToken]
+  );
+
+  if (!user) return false;
+
+  const userId = user.rows[0].user_id;
+
+  const userInfo = await connection.query('SELECT * FROM users WHERE id = $1', [
+    userId,
+  ]);
+
+  if (!userInfo) return false;
+
+  const userClassId = userInfo.rows[0].class_id;
+
+  return { userId, userClassId };
+};
+
+export { regiserUserToken, getUserInfoByToken };
