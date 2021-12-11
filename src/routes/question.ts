13075c8568056199b98a516b04bf13562e348d91
bruch0 <lucas.bruch0@gmail.com>
2021-12-11 13:11:32 -0300

    feat: add vote to the router

--- src/routes/question.ts
+++ src/routes/question.ts
@@ -11,4 +11,8 @@ router.get('/question/:questionId', questionController.getQuestion);
 
 router.get('/questions', questionController.getAllUnansweredQuestions);
 
+router.put('/questions/:questionId/up-vote', questionController.voteQuestion);
+
+router.put('/questions/:questionId/down-vote', questionController.voteQuestion);
+
 export default router;
